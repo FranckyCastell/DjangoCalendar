66b749a2dd034edc9f0dba97cf293ee9e3980878
FranckyCastell <francesc.castell8@gmail.com>
2021-12-12 12:38:13 +0100

    FullCalendar.io BASE

--- CalendarApp/models.py
+++ CalendarApp/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class Event (models.Model):
+    title = models.CharField(verbose_name='Título',
+                             max_length=50, blank=True, null=True)
+
+    start = models.CharField(verbose_name='Data Inicio',
+                             blank=True, null=True, default='2021-12-11T22:30:00', max_length=20)
+
+    end = models.CharField(verbose_name='Data Final',
+                           blank=True, null=True, default='2021-12-11T22:30:00', max_length=20)
+
+    url = models.URLField(verbose_name='URL Google Meet',
+                          blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
+
+    def __str__(self):
+        return self.title
--- CalendarApp/views.py
+++ CalendarApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from CalendarApp.models import Event
 
 # Create your views here.
 
 
 def calendar(request):
-    return render(request, 'CalendarApp/calendar.html')
+
+    events = Event.objects.all()
+
+    context = {'events': events}
+    return render(request, 'CalendarApp/calendar.html', context)
